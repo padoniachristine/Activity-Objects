91084ee635e661e59ee0d71b461377242396a835
padoniachristine <143053303+padoniachristine@users.noreply.github.com>
2023-11-18 02:00:51 -1200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,67 @@
+import { StyleSheet, Text, View, FlatList, ScrollView } from 'react-native';
+import { Button } from '@rneui/themed';
+import { users } from './infos';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+export default function App() {
+  const showFullname = (users) =>{
+    alert(users)
+  };
+
+  return (
+      <View style={styles.container}>
+      <ScrollView>
+      <Entypo/>
+      <FlatList
+        data = {users}
+        keyExtractor = {(index) => index.toString()}
+        renderItem = {({item}) => (
+        <View style = {styles.user}>
+            <View style = {styles.nnCon}>
+              <Text style = {styles.name}>{item.nickname}</Text>
+            </View>
+            <View style = {styles.buttonCont}>
+              <Button title = "View"
+              onPress = {()=>showFullname(`${'Name: '}${item.name.firstname}${' '}${item.name.lastname}${'\n'}${'Course: '}${item.course}${'\n'}${'Year: '}${item.year}`)}
+              buttonStylestyle = {styles.buttonVw}/>
+            </View>
+        </View>     
+        )}
+      />
+      </ScrollView>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems:'center',
+    marginTop:10,
+    padding:10
+  },
+  user: {
+    marginBottom: 10,
+    width:300,
+    flexDirection: 'row',
+  },
+  name: {
+    fontSize: 28,
+    marginTop: 10,
+    fontFamily: 'Times New Roman',
+    fontWeight:'normal',
+  },
+  
+  nnCon:{
+    padding:10,
+    width:200,
+  },
+  buttonCon:{
+    marginTop:10,
+    width:80,
+    flexDirection:'row',
+  },
+  buttonVw:{
+    width:65,
+  }  
+});
---
+++ infos.js
@@ -0,0 +1,48 @@
+export const users = [
+    {
+      nickname:'Tine',
+      name: {
+        firstname:'CHRISTINE',
+        lastname:'PADONIA',
+      },
+      course:'BSIT',
+      year:'3',
+      
+    },
+    {
+      nickname:'Eyang',
+      name: {
+        firstname:'JEA MARIE',
+        lastname:'OCHEA',
+      },
+      course:'BSAMT',
+      year:'1',
+    },
+    {
+      nickname:'Onang',
+      name: {
+        firstname:'JUNAH MARIE',
+        lastname:'MASLOG',
+      },
+      course:'BSCRIM',
+      year:'4',
+    },
+    {
+      nickname:'Kwen',
+      name: {
+        firstname:'QUENNIE',
+        lastname:'EULLONE',
+      },
+      course:'BSFM',
+      year:'4',
+    },
+    {
+      nickname:'Meng',
+      name: {
+        firstname:'FLORA',
+        lastname:'TORRES',
+      },
+      course:'BSHM',
+      year:'4',
+    },
+  ];
